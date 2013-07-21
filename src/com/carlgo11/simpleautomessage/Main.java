5faee857ff8f09ce33dfc0875f48a2264310bcd8
Carlgo11 <Carlgo11@portalcraft.se>
2013-07-22 00:23:06 +0200

    Just updating in order to merge

--- src/com/carlgo11/simpleautomessage/Main.java
+++ src/com/carlgo11/simpleautomessage/Main.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
