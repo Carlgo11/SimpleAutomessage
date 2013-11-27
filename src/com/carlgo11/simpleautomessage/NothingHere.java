4bc5d5ff5130c36b2ec64b73591975fd0f3d60f0
Carlgo11 <Carlgo11@portalcraft.se>
2013-11-27 21:43:54 +0100

    What are yer lookin at huh? There's nothing here. GO AWAY!

--- src/com/carlgo11/simpleautomessage/NothingHere.java
+++ src/com/carlgo11/simpleautomessage/NothingHere.java
@@ -1,6 +1,6 @@
 package com.carlgo11.simpleautomessage;
 
-public class NothingHere {
+public class NothingHere { // There's nothing here. Why are you even looking at this class?
     
     
     public static final String[] playerMoo = new String[]
