85e7007da63626bdd19532991c3e5f7b5d8fd130
Carlgo11 <Carlgo11@portalcraft.se>
2014-02-02 14:26:47 +0100

    Changed default of update-config to false + added new graph

--- src/main/java/com/carlgo11/simpleautomessage/metrics/CustomGraphs.java
+++ src/main/java/com/carlgo11/simpleautomessage/metrics/CustomGraphs.java
@@ -1,75 +1,103 @@
-package com.carlgo11.simpleautomessage.metrics;
+package com.carlgo11.simpleautomessage.Metrics;
 
 import com.carlgo11.simpleautomessage.Main;
 
 public class CustomGraphs {
 
-    public static void graphs(Metrics metrics, Main Main)
-    { // Custom Graphs. Sends data to mcstats.org
+    public static void graphs(Metrics Metrics, Main Main)
+    {
         try {
-            //Graph1
-            Metrics.Graph graph1 = metrics.createGraph("Messages"); //Sends data about how many msg strings the server has.
-            int o = 0;
-            for (int i = 1; Main.getConfig().contains("msg" + i); i++) {
-                o = i;
-            }
-            graph1.addPlotter(new SimplePlotter("" + o));
-
-            //graph2
-            Metrics.Graph graph2 = metrics.createGraph("auto-update"); //Sends auto-update data. if auto-update: is true it returns 'enabled'.
-            if (Main.getConfig().getBoolean("auto-update") == true) {
-                graph2.addPlotter(new SimplePlotter("enabled"));
-            } else {
-                graph2.addPlotter(new SimplePlotter("disabled"));
-            }
-
-            //Graph3
-            Metrics.Graph graph3 = metrics.createGraph("language");
-            if (Main.getConfig().getString("language").equalsIgnoreCase("EN") || Main.getConfig().getString("language").isEmpty()) {
-                graph3.addPlotter(new SimplePlotter("English"));
-            }else
-            if (Main.getConfig().getString("language").equalsIgnoreCase("FR")) {
-                graph3.addPlotter(new SimplePlotter("French"));
-            }else
-            if (Main.getConfig().getString("language").equalsIgnoreCase("NL")) {
-                graph3.addPlotter(new SimplePlotter("Dutch"));
-            }else
-            if (Main.getConfig().getString("language").equalsIgnoreCase("SE")) {
-                graph3.addPlotter(new SimplePlotter("Swedish"));
-            }else
-            if (Main.getConfig().getString("language").equalsIgnoreCase("RU") ) {
-                graph3.addPlotter(new SimplePlotter("Russian"));
-            }else{
-                graph3.addPlotter(new SimplePlotter("Other"));
-            }
-
-            //Graph4
-            Metrics.Graph graph4 = metrics.createGraph("min-players");
-            graph4.addPlotter(new SimplePlotter("" + Main.getConfig().getInt("min-players")));
-
-            //Graph5
-            Metrics.Graph graph5 = metrics.createGraph("random");
-            if (Main.getConfig().getBoolean("random")) {
-                graph5.addPlotter(new SimplePlotter("enabled"));
-            } else {
-                graph5.addPlotter(new SimplePlotter("disabled"));
-            }
-
-            //Graph6
-            Metrics.Graph graph6 = metrics.createGraph("warn-update0");
-            if (!Main.getConfig().getBoolean("auto-update")) {
-                if (Main.getConfig().getString("warn-update").equalsIgnoreCase("op")) {
-                    graph6.addPlotter(new SimplePlotter("op"));
-                } else if (Main.getConfig().getString("warn-update").equalsIgnoreCase("perm")) {
-                    graph6.addPlotter(new SimplePlotter("perm"));
-                } else {
-                    graph6.addPlotter(new SimplePlotter("none"));
-                }
-            }
-            Main.debug("Sending metrics data...");
-            metrics.start();
+            graph1(Metrics, Main);
+            graph2(Metrics, Main);
+            graph3(Metrics, Main);
+            graph4(Metrics, Main);
+            graph5(Metrics, Main);
+            graph6(Metrics, Main);
+            graph7(Metrics, Main);
+            Main.debug("Sending Metrics data...");
+            Metrics.start();
         } catch (Exception e) {
             Main.logger.warning(e.getMessage());
+        }
+
+    }
+
+    void graph1(Metrics Metrics, Main Main) throws Exception
+    {
+        Metrics.Graph graph1 = Metrics.createGraph("Messages"); //Sends data about how many msg strings the server has.
+        int o = 0;
+        for (int i = 1; Main.getConfig().contains("msg" + i); i++) {
+            o = i;
+        }
+        graph1.addPlotter(new SimplePlotter("" + o));
+    }
+
+    void graph2(Metrics Metrics, Main Main) throws Exception
+    {
+        Metrics.Graph graph2 = Metrics.createGraph("auto-update"); //Sends auto-update data. if auto-update: is true it returns 'enabled'.
+        if (Main.getConfig().getBoolean("auto-update") == true) {
+            graph2.addPlotter(new SimplePlotter("enabled"));
+        } else {
+            graph2.addPlotter(new SimplePlotter("disabled"));
+        }
+
+    }
+
+    void graph3(Metrics Metrics, Main Main) throws Exception
+    {
+        Metrics.Graph graph3 = Metrics.createGraph("language");
+        if (Main.getConfig().getString("language").equalsIgnoreCase("EN") || Main.getConfig().getString("language").isEmpty()) {
+            graph3.addPlotter(new SimplePlotter("English"));
+        } else if (Main.getConfig().getString("language").equalsIgnoreCase("FR")) {
+            graph3.addPlotter(new SimplePlotter("French"));
+        } else if (Main.getConfig().getString("language").equalsIgnoreCase("NL")) {
+            graph3.addPlotter(new SimplePlotter("Dutch"));
+        } else if (Main.getConfig().getString("language").equalsIgnoreCase("SE")) {
+            graph3.addPlotter(new SimplePlotter("Swedish"));
+        } else if (Main.getConfig().getString("language").equalsIgnoreCase("RU")) {
+            graph3.addPlotter(new SimplePlotter("Russian"));
+        } else {
+            graph3.addPlotter(new SimplePlotter("Other"));
+        }
+    }
+
+    void graph4(Metrics Metrics, Main Main) throws Exception
+    {
+        Metrics.Graph graph4 = Metrics.createGraph("min-players");
+        graph4.addPlotter(new SimplePlotter("" + Main.getConfig().getInt("min-players")));
+    }
+
+    void graph5(Metrics Metrics, Main Main) throws Exception
+    {
+        Metrics.Graph graph5 = Metrics.createGraph("random");
+        if (Main.getConfig().getBoolean("random")) {
+            graph5.addPlotter(new SimplePlotter("enabled"));
+        } else {
+            graph5.addPlotter(new SimplePlotter("disabled"));
+        }
+    }
+
+    void graph6(Metrics Metrics, Main Main) throws Exception
+    {
+        Metrics.Graph graph6 = Metrics.createGraph("warn-update0");
+        if (!Main.getConfig().getBoolean("auto-update")) {
+            if (Main.getConfig().getString("warn-update").equalsIgnoreCase("op")) {
+                graph6.addPlotter(new SimplePlotter("op"));
+            } else if (Main.getConfig().getString("warn-update").equalsIgnoreCase("perm")) {
+                graph6.addPlotter(new SimplePlotter("perm"));
+            } else {
+                graph6.addPlotter(new SimplePlotter("none"));
+            }
+        }
+    }
+
+    void graph7(Metrics Metrics, Main Main) throws Exception
+    {
+        Metrics.Graph graph7 = Metrics.createGraph("update-config");
+        if (Main.getConfig().getBoolean("update-config")) {
+            graph7.addPlotter(new SimplePlotter("enabled"));
+        } else {
+            graph7.addPlotter(new SimplePlotter("disabled"));
         }
     }
 }
