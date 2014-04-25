39b1465ba250c064d9ed750ff0f31ce2633fa27b
Carlgo11 <carlgo11@carlgo11.com>
2014-04-25 23:09:33 +0200

    Part 2 of major code rebuild

--- src/main/java/com/carlgo11/simpleautomessage/Announce.java
+++ src/main/java/com/carlgo11/simpleautomessage/Announce.java
@@ -1,39 +1,46 @@
 package com.carlgo11.simpleautomessage;
 
-import com.carlgo11.simpleautomessage.language.Lang;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 
 public class Announce {
 
-    static private Main Main;
-    static private int messageCounter;
-    static private int lastMessage = 0;
-    static public boolean isRandom;
+    private Main Main;
+    private int messageCounter = 0;
+    private int lastMessage;
+    private boolean isRandom;
+    private int warningCounter = 0;
 
     public void setup(Main m) {
         this.Main = m;
         this.isRandom = Main.getConfig().getBoolean("random");
         int cm = collectMessages();
-        if (cm == 0) {
-            m.getLogger().severe("Could not load any messages from the config. Did you forget to add any or is the config broken?");
-        }
-        this.lastMessage = collectMessages();
-        schedule();
+        this.lastMessage = cm;
+        schedule(cm);
 
     }
 
-    private void schedule() {
+    private void schedule(final int cm) {
+
         Bukkit.getScheduler().scheduleSyncRepeatingTask(Main, new Runnable() {
 
             public void run() {
-                if (isRandom) {
-                    onRandom();
+
+                if (cm == 0) {
+                    Main.getLogger().severe("Could not load any messages from the config. Did you forget to add any or is the config broken?");
+                    warningCounter++;
+                    if (warningCounter >= 5) {
+                        return;
+                    }
                 } else {
-                    onInOrder();
+                    if (isRandom) {
+                        onRandom();
+                    } else {
+                        onInOrder();
+                    }
                 }
             }
-        }, Main.getConfig().getLong("time"), Main.getConfig().getLong("time"));
+        }, Main.time, Main.time);
     }
 
     private void onRandom() {
@@ -45,6 +52,7 @@ public class Announce {
     private void onInOrder() {
         int nm = getNextMessage();
         String message = Main.getConfig().getString("msg" + nm);
+        System.out.println(nm);
         sendMessage(message);
 
     }
@@ -53,7 +61,9 @@ public class Announce {
         Main.debug("Running message collection query...");
         int currentMessage = 0;
         while (true) {
-            if (!Main.getConfig().contains("msg" + currentMessage++)) {
+            currentMessage++;
+            System.out.println("checkMessage "+currentMessage);
+            if (!Main.getConfig().contains("msg" + currentMessage)) {
                 return currentMessage;
             }
         }
@@ -63,10 +73,13 @@ public class Announce {
         if (isRandom) {
             return Main.getRandomInt(lastMessage);
         } else {
-            int nm = messageCounter++;
-            if (nm <= lastMessage) {
+            int nm = messageCounter+1;
+            System.out.println("nm "+nm+"\tlastMessage "+lastMessage);
+            if (nm < lastMessage) {
+                messageCounter++;
                 return nm;
             } else {
+                messageCounter=1;
                 return 1;
             }
         }
