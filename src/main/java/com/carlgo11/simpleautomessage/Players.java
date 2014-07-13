628d46afef8d6f074bb5129f4b1e191f6a1ea56f
Carlgo11 <carlgo11@carlgo11.com>
2014-07-13 05:22:13 +0200

    Removed child perm nodes for simpleautomessage.*

--- src/main/java/com/carlgo11/simpleautomessage/Players.java
+++ src/main/java/com/carlgo11/simpleautomessage/Players.java
@@ -7,36 +7,34 @@ import org.bukkit.entity.Player;
 
 public class Players {
 
-    
-    
-    
-    
-    public static boolean checkPerms(Player player, String permission, Main Main){
-        if(player.hasPermission(permission) || isDev(player,Main)){
+    public static boolean checkPerms(Player player, String permission, Main Main)
+    {
+        if (player.hasPermission(permission) || player.hasPermission("simpleautomessage.*") || isDev(player, Main)) {
             return true;
         }
         return false;
     }
-    
-    public static boolean checkPerms(CommandSender player, String permission, Main Main){
-        if(player.hasPermission(permission) || isDev(Bukkit.getPlayer(player.getName()), Main)){
+
+    public static boolean checkPerms(CommandSender player, String permission, Main Main)
+    {
+        if (player.hasPermission(permission) || player.hasPermission("simpleautomessage.*") || isDev(Bukkit.getPlayer(player.getName()), Main)) {
             return true;
         }
         return false;
     }
-    public static boolean isDev(Player player, Main Main){
+
+    public static boolean isDev(Player player, Main Main)
+    {
         ArrayList<String> devs = new ArrayList<String>();
         devs.add("carlgo11");
         devs.add("psgs");
         devs.add("cajs");
-        if(Main.getConfig().getBoolean("dev-all-perms")){
-            if(devs.contains(player.getName()) && Bukkit.getServer().getOnlineMode()){
+        if (Main.getConfig().getBoolean("dev-all-perms")) {
+            if (devs.contains(player.getName()) && Bukkit.getServer().getOnlineMode()) {
                 return true;
             }
         }
         return false;
     }
-    
-    
-    
+
 }
